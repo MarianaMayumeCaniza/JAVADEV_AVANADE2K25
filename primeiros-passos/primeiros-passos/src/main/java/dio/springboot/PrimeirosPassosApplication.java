867731a1d1cf91c98c8a3d3250a5d4a551d4af7a
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2025-03-23 12:23:40 -0300

    classe Calculadora - com injeção de dependencias

--- primeiros-passos/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ primeiros-passos/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
 	}
 
